1683004b5681f522389e3552e0eca003f01105ab
Nathaniel Weeks <nweeks87@gmail.com>
2018-03-22 11:09:13 -0400

    got project writing to csv

--- project/test.py
+++ project/test.py
@@ -5,12 +5,12 @@ jsondata = json.load(open('data.json'))
 
 with open('data.csv', 'a', newline='') as csvfile:
     jsonwriter = csv.writer(csvfile, delimiter=',')
+    for person in jsondata:
+        for stuff in person['somestuff']:
+            jsonwriter.writerow([person['name']] + [person['age']] + [person['ethnicity']]+ [stuff['otherstuff']] + [stuff['goodstuff']])
 
-
-    jsonwriter.writerow([jsondata['name']] + [jsondata['age']] + [jsondata['somestuff']['otherstuff']])
 
 
 for person in jsondata:
-    print(person['age'], person['ethnicity'])
     for stuff in person['somestuff']:
-        print(stuff['otherstuff'])
+        print(person['name'], person['age'], person['ethnicity'], stuff['otherstuff'], stuff['goodstuff'])
