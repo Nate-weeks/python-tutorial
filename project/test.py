20796c20bf386cb81c03dbb028d711b5e5205177
Nathaniel Weeks <nweeks87@gmail.com>
2018-03-21 23:07:17 -0400

    worked on project

--- project/test.py
+++ project/test.py
@@ -5,6 +5,12 @@ jsondata = json.load(open('data.json'))
 
 with open('data.csv', 'a', newline='') as csvfile:
     jsonwriter = csv.writer(csvfile, delimiter=',')
-    jsonwriter.writerow([jsondata['name']] + [jsondata['age']])
 
-print(jsondata['name'])
+
+    jsonwriter.writerow([jsondata['name']] + [jsondata['age']] + [jsondata['somestuff']['otherstuff']])
+
+
+for person in jsondata:
+    print(person['age'], person['ethnicity'])
+    for stuff in person['somestuff']:
+        print(stuff['otherstuff'])
